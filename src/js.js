2ecf77fd730a9ec44f1bead00f76a3e54459c8ae
imrankhan18 <imrankhan@cedcoss.com>
2022-02-05 12:36:29 +0530

    subtask22 done

--- src/js.js
+++ src/js.js
@@ -1,56 +1,57 @@
 function javaS() {
-    //document.getElementById("result").innerHTML="Print"+result ;
-    console.log("result"+result);
-    let num=document.getElementById('value').value;
-    var multiply= num*10;
-    document.getElementById('result').innerHTML=multiply;
-    const myFirstName=('Imran')
-    let myLastName=('Khan')//give error because when we use let a variable with the same name can only be declared once.
-    let fullName=myFirstName+myLastName;
-    document.getElementById('str').innerHTML=fullName;
-    console.log(str);
-
-
-
+  //document.getElementById("result").innerHTML="Print"+result ;
+  console.log("result" + result);
+  let num = document.getElementById("value").value;
+  var multiply = num * 10;
+  document.getElementById("result").innerHTML = multiply;
+  const myFirstName = "Imran";
+  let myLastName = "Khan"; //give error because when we use let a variable with the same name can only be declared once.
+  let fullName = myFirstName + myLastName;
+  document.getElementById("str").innerHTML = fullName;
+  console.log(str);
 }
 //tadsk which is done in freecodecamp
 //shows result on console
-function task(){
-  
-    const mul = 45* 33;
-    document.getElementById(mul);
-    console.log(mul);
-    const quotient = 66 / 33;
-    document.getElementById(quotient);
-    console.log(quotient);
+function task() {
+  const mul = 45 * 33;
+  document.getElementById(mul);
+  console.log(mul);
+  const quotient = 66 / 33;
+  document.getElementById(quotient);
+  console.log(quotient);
 
-    let myVar = 12;
-    myVar=--myVar;
-    console.log(myVar);
-    
-    const myDecimal = 5.7;
-    console.log(myDecimal);
+  let myVar = 12;
+  myVar = --myVar;
+  console.log(myVar);
 
-    const product = 2.0 * 2.5;
-    console.log(product);
+  const myDecimal = 5.7;
+  console.log(myDecimal);
 
-    const quotient1 = 4.4 / 2.0;
-    console.log(quotient1);
+  const product = 2.0 * 2.5;
+  console.log(product);
 
-    const remainder = 11%3;
-    console.log(remainder);
+  const quotient1 = 4.4 / 2.0;
+  console.log(quotient1);
 
-    let a = 3;
-    let b = 17;
-    let c = 12;
+  const remainder = 11 % 3;
+  console.log(remainder);
 
-// Only change code below this line
-    a +=  12;
-    b += 9;
-    c += 7;
-    console.log(a,b,c);
+  let a = 3;
+  let b = 17;
+  let c = 12;
 
+  a += 12;
+  b += 9;
+  c += 7;
+  console.log(a, b, c);
 
+  let a1 = 11;
+  let b1 = 9;
+  let c1 = 3;
 
+  a1 -= 6;
+  b1 -= 15;
+  c1 -= 1;
+  console.log(a1, b1, c1);
 }
-task()
+task();
