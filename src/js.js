beb55ea68fbc741330aae7fa015345b6d90fa099
imrankhan18 <imrankhan@cedcoss.com>
2022-02-05 11:16:57 +0530

    subtask6 done

--- src/js.js
+++ src/js.js
@@ -2,8 +2,11 @@ function javaS() {
     //document.getElementById("result").innerHTML="Print"+result ;
     console.log("result"+result);
     let num=document.getElementById('value').value;
-    var multiply= n*10;
-    document.getElementById('result').innerHTML=multiply
+    var multiply= num*10;
+    document.getElementById('result').innerHTML=multiply;
+    var myFirstName=('Imran')
+    var myLastName=('Khan')
+    DocumentTimeline.getElementById('result').innerHTML=''
 
 
 
