7524e2875be93305583d7d19666555682c2c70fd
imrankhan18 <imrankhan@cedcoss.com>
2022-02-05 11:47:24 +0530

    subtask10 done

--- src/js.js
+++ src/js.js
@@ -4,9 +4,9 @@ function javaS() {
     let num=document.getElementById('value').value;
     var multiply= num*10;
     document.getElementById('result').innerHTML=multiply;
-    let myLastName=('Imran')
+    const myFirstName=('Imran')
     let myLastName=('Khan')//give error because when we use let a variable with the same name can only be declared once.
-    var fullName=myLastName+myLastName;
+    let fullName=myFirstName+myLastName;
     document.getElementById('str').innerHTML=fullName;
     console.log(str);
 
