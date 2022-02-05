007791438160e780f773e7edf7772dda6ae98c20
imrankhan18 <imrankhan@cedcoss.com>
2022-02-05 12:29:06 +0530

    subtask21 done

--- src/js.js
+++ src/js.js
@@ -40,6 +40,16 @@ function task(){
     const remainder = 11%3;
     console.log(remainder);
 
+    let a = 3;
+    let b = 17;
+    let c = 12;
+
+// Only change code below this line
+    a +=  12;
+    b += 9;
+    c += 7;
+    console.log(a,b,c);
+
 
 
 }
